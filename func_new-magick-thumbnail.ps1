e327340f7bfa958f3659d4ad0ca91c1457fb14c0
Antonino BotelhoFilho <abotelhofilho@gmail.com>
2025-11-11 18:11:37 -0500

    feat: function file

---
+++ func_new-magick-thumbnail.ps1
@@ -0,0 +1,49 @@
+function new-magick-thumbnails {
+    [CmdletBinding()]
+    Param (
+        [Parameter(Mandatory = $true)]  [String]$path, ## path to directory of images
+        [Parameter(Mandatory = $true)]  [String]$resize ## percent to resize the images
+    )
+    ## check if provided path is good
+    Write-Verbose "Checking path..."
+    if (!(Test-Path $path)) { Write-Host "An error occurred. Path provided doesn't exist. Try again." -ForegroundColor Red }
+    ## check if winget is installed, needed to install ImageMagick in the next check
+    Write-Verbose "Checking if winget is installed..."
+    try {
+        winget | out-null
+    }
+    catch {
+        Write-Host "An error occurred. You don't have winget installed.  Go fix that and come back." -ForegroundColor Red
+    }
+    ## check if ImageMagick is installed, if not installed, it installs ImageMagick.Q16-HDRI
+    Write-Verbose "Checking if ImageMagick is installed..."
+    if (!(winget list --name ImageMagick) -like "ImageMagick Q16-HDRI ImageMagick.Q16-HDRI 7*") {
+        Write-Host "ImageMagick not installed." -ForegroundColor Red
+        Write-Host "Installing ImageMagick..." -ForegroundColor Green
+        winget install ImageMagick.Q16-HDRI
+    }
+    ## check if magick is installed and working
+    Write-Verbose "Checking if magick is installed and working..."
+    try {
+        magick --version | out-null
+    }
+    catch {
+        Write-Host "An error occurred. ImageMagick might not be installed or failed to install. `nTry restarting powershell, if that doesn't fix it, trobleshoot ImageMagick install." -ForegroundColor Red
+    }
+    ## check if the directory path ends with a \ and if it doesn't, add it.
+    if ($path[-1] -notmatch "\`\") { $path = $path + "`\" }
+    ## get list of jpg files from directory
+    Write-Verbose "Getting list of jpg files from provided path..."
+    $imgs = Get-ChildItem -Path $path -File *.jpg
+    ## loop through each file and create thumbnail file
+    Write-Verbose "Looping through each file and creating thumbnail file"
+    foreach (
+        $img in $imgs
+    ) { 
+        ## set thumbnail file name
+        Write-Verbose "Creating thumbnail file name..."
+        $fileSmlName = $path + $img.BaseName + "_thumb.jpg"
+        Write-Verbose "$fileSmlName"
+        magick $img.FullName -resize $resize $fileSmlName 
+    }
+}
